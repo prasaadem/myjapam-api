aecf1e8ba6da9c8bbfe5f690e01afd33993224a8
holoaditya <aditya@hologramsciences.com>
2023-11-22 01:48:39 -0600

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('Home Page');
+  res.send('Home Page welcome');
 });
 
 export default router;
