3e545c0436669e9a39879a6d1c8524a9d0af9a24
holoaditya <aditya@hologramsciences.com>
2023-11-22 03:57:22 -0600

    Updated index page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,24 @@ import { connectToDatabase } from '../db';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('Home Page');
+  const htmlResponse = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>My Japam</title>
+    </head>
+    <body>
+      <h1>Welcome to My Japam App</h1>
+      <p>This is a simple app that let's uses create Japams and keep track of their japams.</p>
+      <p>Check the <a href="/check-db">/check-db</a> endpoint to test the database connection.</p>
+    </body>
+    </html>
+  `;
+
+  res.status(200).send(htmlResponse);
 });
 
 // Check database connection
